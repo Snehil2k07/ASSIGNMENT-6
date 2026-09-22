590a2aaf19302ed653a3d1d1815d556ade91afbc
Snehil2k07 <Snehil2k07@gmail.com>
2026-09-22 22:56:10 +0530

    add q7 program

---
+++ q7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, remainder;
+
+    printf("Enter a positive decimal integer: ");
+    scanf("%d", &n);
+
+    printf("Binary = ");
+
+    while (n > 0)
+    {
+        remainder = n % 2;
+        printf("%d", remainder);
+        n = n / 2;
+    }
+
+    return 0;
+}
