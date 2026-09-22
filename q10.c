ab0ca73b79cd7300f83aaf29080f0a2f6a432d63
Snehil2k07 <Snehil2k07@gmail.com>
2026-09-22 22:58:54 +0530

    add q10 program

---
+++ q10.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, space, num;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        /* Print spaces */
+        for (space = 0; space < n - i - 1; space++)
+        {
+            printf(" ");
+        }
+
+        num = 1;
+
+        /* Print numbers */
+        for (j = 0; j <= i; j++)
+        {
+            printf("%d ", num);
+
+            num = num * (i - j) / (j + 1);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
